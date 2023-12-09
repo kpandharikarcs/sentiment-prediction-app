ef4c7bfa25a0e5e9b35a6b56081bfc9029e0c532
Kalyani Pandharikar <kpandharikarcs@gmail.com>
2023-12-08 18:32:14 -0600

    change python version

--- app.py
+++ app.py
@@ -17,5 +17,6 @@ def predict():
         prediction = model.predict([review])[0]
         return render_template('index.html', prediction=prediction, review=review)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
