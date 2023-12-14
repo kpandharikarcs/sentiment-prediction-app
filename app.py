247c0f884f55c6e15fa77f4a143ea697d22fe66b
Kalyani Pandharikar <kpandharikarcs@gmail.com>
2023-12-13 22:19:17 -0600

    Adding comments to the code

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import os
 
 app = Flask(__name__)
 
-# Load the trained model
+# Load the trained model generated in notebook code
 model = joblib.load('sentiment_model.joblib')
 
+#rendering html code on application
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#code responsible for HTTP POST for review extraction
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
@@ -18,6 +20,7 @@ def predict():
         prediction = model.predict([review])[0]
         return render_template('index.html', prediction=prediction, review=review)
 
+#making the port dynamic
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
